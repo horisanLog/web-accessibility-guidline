b0c878a2dc7b959987fd74401181a43b4ecb1d02
horisanLog <ttie7374@gmail.com>
2022-06-19 10:19:09 +0900

    mainタグの追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,11 @@ import React, { memo } from "react";
 
 export const TopPage: React.FC = memo(() => {
   return (
-    <h1>TOP</h1>
-  )
+    <>
+      <main>
+        <h1>TOP</h1>
+        <div>こんにちは</div>
+      </main>
+    </>
+  );
 });
